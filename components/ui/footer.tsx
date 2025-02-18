065a6cc97fd43447a98e610e5d70637eee6dcc59
naureen163 <118357909+naureen163@users.noreply.github.com>
2025-02-18 21:49:16 +0530

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 mt-20">
       <div className="container mx-auto text-center">
-        <p>MMS School Website</p>
+        <p>MMS School</p>
       </div>
     </footer>
   );
