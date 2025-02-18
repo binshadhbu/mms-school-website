2fe618525581f84c85195af6ccb43bef918349db
Naureen <naureenrkh@gmail.com>
2025-02-18 21:46:43 +0530

    Added footer component

---
+++ components/ui/footer.tsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4 mt-20">
+      <div className="container mx-auto text-center">
+        <p>MMS School Website</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
